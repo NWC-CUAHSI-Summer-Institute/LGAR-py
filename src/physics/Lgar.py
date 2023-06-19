5f80af99d2d50032a1dc1ced7dfc43aa76d12de6
taddyb <tkb5476@psu.edu>
2023-06-19 10:53:06 -0500

    FINISHED VAR CHECK

--- src/physics/Lgar.py
+++ src/physics/Lgar.py
@@ -121,30 +121,28 @@ class LGAR:
         self.sft_coupled = False
         self.use_closed_form_G = False
 
-        # Setting the parameter check variables to False
-        is_layer_thickness_set = False
-        is_initial_psi_set = False
-        is_timestep_set = False
-        is_endtime_set = False
-        is_forcing_resolution_set = False
-        is_layer_soil_type_set = False
-        is_wilting_point_psi_cm_set = False
-        is_soil_params_file_set = False
-        is_max_soil_types_set = False
-        is_giuh_ordinates_set = False
-        is_soil_z_set = False
-        is_ponded_depth_max_cm_set = False
+        # # Setting the parameter check variables to False
+        # is_layer_thickness_set = False
+        # is_initial_psi_set = False
+        # is_timestep_set = False
+        # is_endtime_set = False
+        # is_forcing_resolution_set = False
+        # is_layer_soil_type_set = False
+        # is_wilting_point_psi_cm_set = False
+        # is_soil_params_file_set = False
+        # is_max_soil_types_set = False
+        # is_giuh_ordinates_set = False
+        # is_soil_z_set = False
+        # is_ponded_depth_max_cm_set = False
 
         # Setting soil parameters based on the cfg file
-        self.layer_thickness_cm = torch.zeros(
-            [len(cfg.data.layer_thickness) + 1], dtype=torch.float64, device=self.device
-        )
-        self.layer_thickness_cm[1:] = torch.tensor(
+        self.layer_thickness_cm = torch.tensor(
             cfg.data.layer_thickness, dtype=torch.float64, device=self.device
         )
         self.cum_layer_thickness = torch.zeros(
-            [len(cfg.data.layer_thickness) + 1], dtype=torch.float64, device=self.device
+            [len(cfg.data.layer_thickness)], dtype=torch.float64, device=self.device
         )
+        self.cum_layer_thickness[0] = self.layer_thickness_cm[0]
         for i in range(1, self.cum_layer_thickness.shape[0]):
             self.cum_layer_thickness[i] = (
                 self.cum_layer_thickness[i - 1].clone() + self.layer_thickness_cm[i]
@@ -153,7 +151,7 @@ class LGAR:
             len(cfg.data.layer_thickness), dtype=torch.float64, device=self.device
         )
         self.soil_depth_cm = self.cum_layer_thickness[-1]
-        is_layer_thickness_set = True
+        # is_layer_thickness_set = True
 
         log.debug(f"Number of Layers: {self.num_layers}")
         [
@@ -164,7 +162,7 @@ class LGAR:
         self.initial_psi = torch.tensor(
             cfg.data.initial_psi, dtype=torch.float64, device=self.device
         )
-        is_initial_psi_set = True
+        # is_initial_psi_set = True
 
         timestep_unit = cfg.data.units.timestep_h[0]
         _func_ = division_switcher.get(timestep_unit, seconds_to_hours)
@@ -172,7 +170,7 @@ class LGAR:
             torch.tensor(cfg.data.timestep, dtype=torch.float64, device=self.device)
         )
         assert self.timestep_h > 0
-        is_timestep_set = True
+        # is_timestep_set = True
 
         endtime_unit = cfg.data.units.endtime_s[0]
         _func_ = multiplication_switcher.get(endtime_unit, seconds_to_seconds)
@@ -180,7 +178,7 @@ class LGAR:
             torch.tensor(cfg.data.endtime, dtype=torch.float64, device=self.device)
         )
         assert self.endtime_s > 0
-        is_endtime_set = True
+        # is_endtime_set = True
 
         forcing_unit = cfg.data.units.forcing_resolution_h[0]
         _func_ = division_switcher.get(forcing_unit, seconds_to_hours)
@@ -190,43 +188,64 @@ class LGAR:
             )
         )
         assert self.forcing_resolution_h > 0
-        is_forcing_resolution_set = True
+        # is_forcing_resolution_set = True
 
         self.ponded_depth_max_cm = torch.max(
             torch.tensor(
                 cfg.data.ponded_depth_max, dtype=torch.float64, device=self.device
             )
         )
-        is_ponded_depth_max_cm_set = True
+        # is_ponded_depth_max_cm_set = True
 
         self.use_closed_form_G = cfg.data.use_closed_form_G
 
-        self.layer_soil_type = torch.zeros(
-            [len(cfg.data.layer_soil_type) + 1], dtype=torch.float64, device=self.device
-        )
-        self.layer_soil_type[1:] = torch.tensor(
+        self.layer_soil_type = torch.tensor(
             cfg.data.layer_soil_type, dtype=torch.float64, device=self.device
         )
-        is_layer_soil_type_set = True
+        # is_layer_soil_type_set = True
 
         self.num_soil_types = torch.tensor(
             cfg.data.max_soil_types, dtype=torch.float64, device=self.device
         )
-        is_max_soil_types_set = True
+        # is_max_soil_types_set = True
 
         self.wilting_point_psi = torch.tensor(
             cfg.data.wilting_point_psi, dtype=torch.float64, device=self.device
         )
-        is_wilting_point_psi_cm_set = True
+        # is_wilting_point_psi_cm_set = True
 
-        self.giuh_ordinates = torch.zeros(
-            [len(cfg.data.giuh_ordinates) + 1], dtype=torch.float64, device=self.device
-        )
-        self.giuh_ordinates[1:] = torch.tensor(
+        self.giuh_ordinates = torch.tensor(
             cfg.data.giuh_ordinates, dtype=torch.float64, device=self.device
         )
         self.num_giuh_ordinates = torch.tensor(
             len(cfg.data.giuh_ordinates), dtype=torch.float64, device=self.device
         )
 
+        # Read in soils information
         self.soils_df = read_soils_file(cfg)
+
+        self.soil_temperature = torch.tensor(0, dtype=torch.float64, device=self.device)
+        self.soil_temperature_z = torch.tensor(
+            0, dtype=torch.float64, device=self.device
+        )
+        self.num_cells_z = torch.tensor(1, dtype=torch.float64, device=self.device)
+
+        # add 1.0e-08 to prevent truncation error
+        # TODO ADD MORE TESTING TO THIS INTERVAL
+        self.forcing_interval = torch.div(
+            self.forcing_resolution_h, (self.timestep_h + 1.0e-08)
+        )
+
+        self.frozen_factor = torch.ones(
+            int(self.num_layers.item()), dtype=torch.float64, device=self.device
+        )
+
+        self.initialize_wetting_front()
+
+    def initialize_wetting_front(self):
+        """
+        calculates initial theta (soil moisture content) and hydraulic conductivity
+        from the prescribed psi value for each of the soil layers
+        :return:
+        """
+        return 0
