25b513c29fa92e8fd55b1b1351bfe163b6a9da1c
taddyb <tkb5476@psu.edu>
2023-06-23 09:49:00 -0500

    added files for differentiable framework

---
+++ lgar-torch/agents/DifferentiableLGAR.py
@@ -0,0 +1,78 @@
+import logging
+from omegaconf import DictConfig
+import torch
+import time
+
+from agents.base import BaseAgent
+
+log = logging.getLogger("agents.DifferentiableLGAR")
+
+
+class DifferentiableLGAR(BaseAgent):
+    def __init__(self, cfg: DictConfig) -> None:
+        """
+        Initialize the Differentiable LGAR code
+
+        Sets up the initial state of the agent
+        :param cfg:
+        """
+        super().__init__()
+
+        self.cfg = cfg
+        torch.manual_seed(0)
+
+        self.criterion = torch.nn.MSELoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr = cfg)
+        pass
+
+    def run(self):
+        """
+        The main operator
+        :return:
+        """
+        raise NotImplementedError
+
+    def train(self):
+        """
+        Main training loop
+        :return:
+        """
+        raise NotImplementedError
+
+    def train_one_epoch(self):
+        """
+        One epoch of training
+        :return:
+        """
+        raise NotImplementedError
+
+    def validate(self):
+        """
+        One cycle of model validation
+        :return:
+        """
+        raise NotImplementedError
+
+    def finalize(self):
+        """
+        Finalizes all the operations of the 2 Main classes of the process, the operator and the data loader
+        :return:
+        """
+        raise NotImplementedError
+
+    def load_checkpoint(self, file_name):
+        """
+        Latest checkpoint loader
+        :param file_name: name of the checkpoint file
+        :return:
+        """
+        raise NotImplementedError
+
+    def save_checkpoint(self, file_name="checkpoint.pth.tar", is_best=0):
+        """
+        Checkpoint saver
+        :param file_name: name of the checkpoint file
+        :param is_best: boolean flag to indicate whether current checkpoint's metric is the best so far
+        :return:
+        """
+        raise NotImplementedError
